2cf21c3445dae514523cd60df28902fedff298a3
alizeruncaged <saylent1@gmail.com>
2025-06-13 02:54:08 +0800

    fixed phone margin on home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -409,7 +409,7 @@ const Home = () => {
 							},
 							backgroundRepeat: { sm: "no-repeat", xs: "no-repeat" },
 							backgroundPosition: "center",
-							marginTop: { lg: "98px", md: "90px", sm: "70px", xs: "65px" },
+							marginTop: { lg: "98px", md: "90px", sm: "70px", xs: "0px" },
 						}}
 					></Box>
 
