8226ddd32089f9dfff4a920d4ecf4b719c3cba21
Vansh Baldha <baldhavansh2505@gmail.com>
2025-12-26 12:38:56 +0530

    changes

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
-const SOCKET_URL = "http://localhost:4000"
+const SOCKET_URL = "https://swing-social-socket-server.onrender.com/"
 
 export const getSocket = (): Socket => {
     if (!socket) {
